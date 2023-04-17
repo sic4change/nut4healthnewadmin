1b85e90893795351e3b5cb177e066bc64d8cb64d
Aaron Asencio <aaron.asencio.tavio@gmail.com>
2023-04-17 12:32:09 +0100

    prepare to filter by point

--- lib/src/features/contracts/presentation/contracts_by_point_and_date.dart
+++ lib/src/features/contracts/presentation/contracts_by_point_and_date.dart
@@ -55,6 +55,8 @@ class _StatisticContractsByPointAndDatePageState extends SampleViewState
 
   /// Translate names
   late String _title;
+  late String _filter;
+  late String _points;
 
   _saveContracts(AsyncValue<List<ContractStadistic>>? contracts) {
     splineSeriesData = <ChartSampleData>[];
@@ -75,6 +77,8 @@ class _StatisticContractsByPointAndDatePageState extends SampleViewState
     super.initState();
     selectedLocale = model.locale.toString();
     _title = 'Diagnósticos';
+    _filter = 'Filtrar';
+    _points = 'Puestos de Salud';
 
   }
 
@@ -109,12 +113,18 @@ class _StatisticContractsByPointAndDatePageState extends SampleViewState
     switch (selectedLocale) {
       case 'en_US':
         _title = 'Diagnosis';
+        _filter = 'Filter';
+        _points = 'Health Points';
         break;
       case 'es_ES':
         _title = 'Diagnósticos';
+        _filter = 'Filtrar';
+        _points = 'Puestos de Salud';
         break;
       case 'fr_FR':
         _title = 'Diagnostics';
+        _filter = 'Filtrer';
+        _points = 'Points de santé';
         break;
     }
     rangeController = RangeController(
@@ -176,81 +186,89 @@ class _StatisticContractsByPointAndDatePageState extends SampleViewState
         color:
         model.isWebFullView ? model.cardThemeColor : model.cardThemeColor,
         child: Center(
-          child: Column(
-            children: <Widget>[
-              Expanded(
-                child: Container(
-                    width: mediaQueryData.orientation == Orientation.landscape
-                        ? model.isWebFullView
-                        ? mediaQueryData.size.width * 0.7
-                        : mediaQueryData.size.width
-                        : mediaQueryData.size.width,
-                    padding: const EdgeInsets.fromLTRB(5, 20, 15, 25),
-                    child: splineChart),
-              ),
-              SfRangeSelectorTheme(
-                  data: SfRangeSelectorThemeData(
-                      activeLabelStyle: TextStyle(
-                          fontSize: 10,
-                          color: isLightTheme ? Colors.black : Colors.white),
-                      inactiveLabelStyle: TextStyle(
-                          fontSize: 10,
-                          color: isLightTheme
-                              ? Colors.black
-                              : const Color.fromRGBO(170, 170, 170, 1)),
-                      activeTrackColor: const Color.fromRGBO(255, 125, 30, 1),
-                      inactiveRegionColor: isLightTheme
-                          ? Colors.white.withOpacity(0.75)
-                          : const Color.fromRGBO(33, 33, 33, 0.75),
-                      thumbColor: Colors.white,
-                      thumbStrokeColor: const Color.fromRGBO(255, 125, 30, 1),
-                      thumbStrokeWidth: 2.0,
-                      overlayRadius: 1,
-                      overlayColor: Colors.transparent),
-                  child: Container(
-                    margin: EdgeInsets.zero,
-                    padding: EdgeInsets.zero,
-                    width: mediaQueryData.orientation == Orientation.landscape
-                        ? model.isWebFullView
-                        ? mediaQueryData.size.width * 0.7
-                        : mediaQueryData.size.width
-                        : mediaQueryData.size.width,
-                    child: Center(
-                      child: Padding(
-                        padding: const EdgeInsets.fromLTRB(14, 0, 15, 15),
-                        child: SfRangeSelector(
-                          min: min,
-                          max: max,
-                          interval: 1,
-                          enableDeferredUpdate: enableDeferredUpdate,
-                          deferredUpdateDelay: 1000,
-                          labelPlacement: LabelPlacement.betweenTicks,
-                          dateIntervalType: DateIntervalType.months,
-                          controller: rangeController,
-                          showTicks: true,
-                          showLabels: true,
-                          dragMode: SliderDragMode.both,
-                          labelFormatterCallback:
-                              (dynamic actualLabel, String formattedText) {
-                            String label = DateFormat.MMM().format(actualLabel);
-                            label = (model.isWebFullView &&
-                                mediaQueryData.size.width <= 1000)
-                                ? label[0]
-                                : label;
-                            return label;
-                          },
-                          onChanged: (SfRangeValues values) {},
-                          child: Container(
-                            height: 75,
-                            padding: EdgeInsets.zero,
-                            margin: EdgeInsets.zero,
-                            child: columnChart,
+          child: SingleChildScrollView(
+            child: Container(
+              color: isLightTheme ? const Color.fromRGBO(250, 250, 250, 1) : null,
+              padding: model.isWebFullView
+                  ? const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 20.0)
+                  : const EdgeInsets.fromLTRB(10.0, 12.5, 10.0, 10.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  //_buildPoints(),
+                  Container(
+                      width: mediaQueryData.orientation == Orientation.landscape
+                          ? model.isWebFullView
+                          ? mediaQueryData.size.width * 0.7
+                          : mediaQueryData.size.width
+                          : mediaQueryData.size.width,
+                      padding: const EdgeInsets.fromLTRB(5, 20, 15, 25),
+                      child: splineChart),
+                  SfRangeSelectorTheme(
+                      data: SfRangeSelectorThemeData(
+                          activeLabelStyle: TextStyle(
+                              fontSize: 10,
+                              color: isLightTheme ? Colors.black : Colors.white),
+                          inactiveLabelStyle: TextStyle(
+                              fontSize: 10,
+                              color: isLightTheme
+                                  ? Colors.black
+                                  : const Color.fromRGBO(170, 170, 170, 1)),
+                          activeTrackColor: const Color.fromRGBO(255, 125, 30, 1),
+                          inactiveRegionColor: isLightTheme
+                              ? Colors.white.withOpacity(0.75)
+                              : const Color.fromRGBO(33, 33, 33, 0.75),
+                          thumbColor: Colors.white,
+                          thumbStrokeColor: const Color.fromRGBO(255, 125, 30, 1),
+                          thumbStrokeWidth: 2.0,
+                          overlayRadius: 1,
+                          overlayColor: Colors.transparent),
+                      child: Container(
+                        margin: EdgeInsets.zero,
+                        padding: EdgeInsets.zero,
+                        width: mediaQueryData.orientation == Orientation.landscape
+                            ? model.isWebFullView
+                            ? mediaQueryData.size.width * 0.7
+                            : mediaQueryData.size.width
+                            : mediaQueryData.size.width,
+                        child: Center(
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(14, 0, 15, 15),
+                            child: SfRangeSelector(
+                              min: min,
+                              max: max,
+                              interval: 1,
+                              enableDeferredUpdate: enableDeferredUpdate,
+                              deferredUpdateDelay: 1000,
+                              labelPlacement: LabelPlacement.betweenTicks,
+                              dateIntervalType: DateIntervalType.months,
+                              controller: rangeController,
+                              showTicks: true,
+                              showLabels: true,
+                              dragMode: SliderDragMode.both,
+                              labelFormatterCallback:
+                                  (dynamic actualLabel, String formattedText) {
+                                String label = DateFormat.MMM().format(actualLabel);
+                                label = (model.isWebFullView &&
+                                    mediaQueryData.size.width <= 1000)
+                                    ? label[0]
+                                    : label;
+                                return label;
+                              },
+                              onChanged: (SfRangeValues values) {},
+                              child: Container(
+                                height: 75,
+                                padding: EdgeInsets.zero,
+                                margin: EdgeInsets.zero,
+                                child: columnChart,
+                              ),
+                            ),
                           ),
                         ),
-                      ),
-                    ),
-                  )),
-            ],
+                      )),
+                ],
+              ),
+            ),
           ),
         ));
     return Scaffold(
@@ -262,6 +280,136 @@ class _StatisticContractsByPointAndDatePageState extends SampleViewState
         ),
       )
           : page,
+    );
+  }
+
+  Widget _buildPoints() {
+    return Column(children: <Widget>[
+      Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: Card(
+          elevation: 2,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(3.0),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 10.0),
+            child: SizedBox(
+              height: 327,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 5, left: 8),
+                    child: Text(
+                      '$_points:',
+                      style: const TextStyle(fontSize: 20),
+                      textAlign: TextAlign.start,
+                    ),
+                  ),
+                 CheckboxListTile(
+                    contentPadding: const EdgeInsets.only(left: 8),
+                    title: const Text(
+                      'Basic',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    value: true,
+                    activeColor: Colors.grey,
+                    onChanged: (bool? value) {},
+                  ),
+                  /*
+                  CheckboxListTile(
+                      contentPadding: const EdgeInsets.only(left: 8),
+                      title: const Text(
+                        'Banquet hall',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      value: _needBanquetHall,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          _needBanquetHall = value!;
+                        });
+                      }),
+                  CheckboxListTile(
+                      contentPadding: const EdgeInsets.only(left: 8),
+                      title: const Text(
+                        'Health spa',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      value: _needHealthSpa,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          _needHealthSpa = value!;
+                        });
+                      }),
+                  CheckboxListTile(
+                      contentPadding: const EdgeInsets.only(left: 8),
+                      title: const Text(
+                        'Pets allowed',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      value: _needPets,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          _needPets = value!;
+                        });
+                      }),
+                  CheckboxListTile(
+                      contentPadding: const EdgeInsets.only(left: 8),
+                      title: const Text(
+                        'Indoor entertainment',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      value: _needIndoorEntertainment,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          _needIndoorEntertainment = value!;
+                        });
+                      }),*/
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 0.0),
+        child: _buildFilterButton(),
+      )
+    ]);
+  }
+
+  Widget _buildFilterButton() {
+    return SizedBox(
+      width: 350,
+      height: 50,
+      child: ElevatedButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(
+              const Color.fromRGBO(255, 102, 102, 1.0)),
+        ),
+
+        /// On clicking the button, chart inside the range selector will be
+        /// updated with the filtered data.
+        onPressed: () {
+          setState(() {
+            /*_updateChartData(
+              _values,
+              hasBanquetHall: _needBanquetHall,
+              hasHealthSpa: _needHealthSpa,
+              arePetsAllowed: _needPets,
+              hasIndoorEntertainment: _needIndoorEntertainment,
+            );*/
+          });
+        },
+        child: Text(
+            _filter,
+          style: const TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
     );
   }
 
